c50c7103658152ed7166fdd2267e59d29711a3ed
renalex <aleksandr@codetime.am>
2023-10-26 00:29:44 +0400

    Adding Communication Microservice

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { join } from 'path';
+
+const fileName = process.env.NODE_ENV ? process.env.NODE_ENV : '';
+const envFilePath = join(__dirname, `../../${fileName}.env`);
+const ENV = config({ path: envFilePath }).parsed;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(ENV.GATEWAY_PORT || 3000);
+
+  console.log(`
+  🚀 Nest.js REST API - Gateway - , launched at http://${ENV.GATEWAY_HOST}:${ENV.GATEWAY_PORT}`);
 }
 bootstrap();
